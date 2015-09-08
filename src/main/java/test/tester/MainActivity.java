f8d16293332ce6470c5883d96207c05334fe4b0b
sumireaio <mblykarl@gmail.com>
2015-09-08 16:38:32 +0800

    created explore initial layout

--- src/main/java/test/tester/MainActivity.java
+++ src/main/java/test/tester/MainActivity.java
@@ -26,7 +26,22 @@ public class MainActivity extends TabActivity {
         TabHost.TabSpec tabspec2 = tabHost.newTabSpec("page2");
         tabspec2.setIndicator("Activities");
         tabspec2.setContent(new Intent(this, Activities.class));
-        tabHost.addTab(tabspec1);
+        tabHost.addTab(tabspec2);
+
+        TabHost.TabSpec tabspec3 = tabHost.newTabSpec("page3");
+        tabspec3.setIndicator("Post");
+        tabspec3.setContent(new Intent(this, Post.class));
+        tabHost.addTab(tabspec3);
+
+        TabHost.TabSpec tabspec4 = tabHost.newTabSpec("page4");
+        tabspec4.setIndicator("Inbox");
+        tabspec4.setContent(new Intent(this, Inbox.class));
+        tabHost.addTab(tabspec4);
+
+        TabHost.TabSpec tabspec5 = tabHost.newTabSpec("page5");
+        tabspec5.setIndicator("Profile");
+        tabspec5.setContent(new Intent(this, Profile.class));
+        tabHost.addTab(tabspec5);
     }
 
     @Override
