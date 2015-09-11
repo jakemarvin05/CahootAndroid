d6b44deaee726ed4ab58e72db2fc8d1afa4049d9
sumireaio <mblykarl@gmail.com>
2015-09-11 16:04:12 +0800

    Fixed the mainactivity actionbar

--- src/main/java/test/tester/MainActivity.java
+++ src/main/java/test/tester/MainActivity.java
@@ -1,10 +1,12 @@
 package test.tester;
 
+import android.app.ActionBar;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TabHost;
 
@@ -50,8 +52,10 @@ public class MainActivity extends TabActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
